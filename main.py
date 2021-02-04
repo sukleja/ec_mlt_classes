63edbba878c1cb195d224421cf935be735dc8334
sebastian.ukleja <sebastian.ukleja@fh-campuswien.ac.at>
2021-02-04 12:10:35 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,19 @@
+
+class car:
+    def __init__(self, color, brand):
+        self.color=color
+        self.brand=brand
+
+    def accelerate(self):
+        print(f"The {self.color} {self.brand} accelerates")
+
+    def crash(self, object):
+        print(f"The {self.color} {self.brand} crashed against a {object}!")
+
+
+if __name__ == '__main__':
+    first_car=car("red","audi")
+    first_car.accelerate()
+    first_car.crash("Wall")
+
+
